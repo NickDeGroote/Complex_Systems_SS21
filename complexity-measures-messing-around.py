4983f9b7af0f200d4226fc00372868ff3634cd7f
Owen <traubeod@mail.uc.edu>
2021-03-15 09:06:47 -0400

    added untested fractal complexity, will try to get these tested and working

--- complexity-measures-messing-around.py
+++ complexity-measures-messing-around.py
@@ -87,6 +87,72 @@ def getClusters(array, height, width):
                     if(not newCellFound):
                         toVisit.pop()
                                 
+                     
+#maybe make a constant so it's not always a power of 2
+#maybe a power of 3 would work better due to the moore neighborhood
+def getFractalComplexity(array, height, width, R1):
+    #R1 is the side length in number of cells of the least magnification
+    #it is assumed that the greatest magnification will be the individual cell
+    #ideally it is a power of 2, and if it's not we pretend it is anyway
+    #k is the number of different scales we can look at 
+    k = (int)(round(log(R1, 2)))
+
+    #create a list in which to hold all the tesselated arrays and the number of squares it takes
+    #to cover the live cells of the automaton at each level
+    scaleArrays = []
+    
+    scaleH = height
+    scaleW = width
+    
+    #start it off with the greatest magnification and work up
+    scaleArrays.append(np.array(array))
+    #create empty list to hold number of sqares at each magnification level
+    numSquares = []    
+    
+    #for each magnification level
+    for i in range(k+1):
+            #start with zero squares counted at this magnification
+            numSquares.append(0)
+            #if you're not at the base/lowest magnification
+            #create the tesselation for the next lowest magnification level
+            scaleH = height/pow(2, i)
+            scaleW = width/pow(2, i)
+            if(not i==k):
+            scaleH = height/pow(2, i)
+            scaleW = width/pow(2, i)        
+            scaleArrays.append[np.zeros(scaleH, scaleW), 0]
+                scaleArrays.append(np.zeros(scaleH, scaleW))
+                    
+                #for your magnification level
+                #this is in here so you don't have to check a million times whether this is the kth iteration
+                for y in range(2 * scaleH):
+                    for x in range(2 * scaleW):
+                        #if this square is occupied at this magnification
+                        if(scaleArrays[i][y][x]):
+                            #increment the number of squares used at this level
+                            numSquares[i] += 1
+                            #set the corresponding square at the next lowest magnification to occupied
+                            #doesn't matter how high this is as long as it's not 0, it's occupied
+                            scaleArrays[i+1][(int)floor(y/2)][(int)floor(x/2)] += 1
+            else:
+                 for y in range(2 * scaleH):
+                    for x in range(2 * scaleW):
+                        if(scaleArrays[i][y][x]):
+                            numSquares[i] += 1
+                         
+     dimensions = []
+     
+     for i in range(k):
+         #calculate s epsilon for each differential magnification level
+         sEp = numSquares[i]/numSquares[i+1]
+         #get rid of the quotient by taking log base 2, log2(2) = 1
+         #and magnification is 2x
+         dimension = log(sEp, 2)
+         dimensions.append(dimension)
+         
+            
+                                
+                                
                                 
                                 
                                 
