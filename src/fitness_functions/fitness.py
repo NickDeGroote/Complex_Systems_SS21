ad04d7ed4498a8c68db9c1d004a29ea1fbdf8331
Owen <traubeod@mail.uc.edu>
2021-03-25 20:08:48 -0400

    added cluster distance fitness, pass initial conditions and timesteps and get a fitness score based on the sum mean distance from center of all the clusters of size greater than 6

--- src/fitness_functions/fitness.py
+++ src/fitness_functions/fitness.py
@@ -1,18 +1,30 @@
 from ConwayGOL import run_GOL
 
 from fractal_fitness import defaultFractalFitness
+from cluster_fitness import defaultClusterDistanceFitness
 import math
 import statistics as stats
 import numpy as np
 
 
-def fractalFitness(initialConditions, timesteps):
+def getGOLResults(initialConditions, timesteps):
     squaredDimensions = len(initialConditions)
     height = int(math.sqrt(squaredDimensions))
     width = height
     initialBoard= np.array(initialConditions)
     result2d, result1d = run_GOL(initialBoard, timesteps, width, height)
+    return(result2d)
+
+def fractalFitness(initialConditions, timesteps):
+    result2d = getGOLResults(initialConditions, timesteps)
     fitnessList = defaultFractalFitness(result2d)
+    meanFitness = stats.mean(fitnessList)
+    return(meanFitness)
+
+def clusterDistanceFitness(initialConditions, timesteps):
+    result2d = getGOLResults(initialConditions, timesteps)
+    fitnessList = defaultClusterDistanceFitness(result2d)
+    print(fitnessList)
     meanFitness = stats.mean(fitnessList)
     return(meanFitness)
 
